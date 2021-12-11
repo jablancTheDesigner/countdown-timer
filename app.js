6042d0e8e2614827996cc00af077f4060786da79
Jahmal Blanchard <45908841+jablancTheDesigner@users.noreply.github.com>
2021-12-11 15:52:06 -0500

    Created readme and app logic

---
+++ app.js
@@ -0,0 +1,33 @@
+const counter = setInterval(() => {
+    countDownTimer()
+}, 1000)
+
+function countDownTimer() {
+    const toDateStr = "12/25/2021";
+    const toDate = new Date(toDateStr).getTime()
+    const now = new Date().getTime();
+    const diff = toDate - now
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000)
+
+    const secondsEl = document.querySelector('.js-seconds')
+    const minutesEl = document.querySelector('.js-minutes')
+    const hoursEl = document.querySelector('.js-hours')
+    const daysEl = document.querySelector('.js-days')
+
+    if (diff >= 0) {
+        secondsEl.innerHTML = seconds
+        minutesEl.innerHTML = minutes
+        hoursEl.innerHTML = hours
+        daysEl.innerHTML = days
+    } else {
+        clearInterval(counter)
+    }
+
+}
+
+countDownTimer()
+
